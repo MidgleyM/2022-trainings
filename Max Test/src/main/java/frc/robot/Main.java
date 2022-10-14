ce986a159ede6f807f7047c45806a92ca5f69a8a
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2022-10-13 19:56:45 -0500

    did stuff

--- Max Test/src/main/java/frc/robot/Main.java
+++ Max Test/src/main/java/frc/robot/Main.java
@@ -3,9 +3,10 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-
+import javax.sound.sampled.SourceDataLine;
+import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.RobotBase;
-
+import frc.robot.subsystems.Square.*;
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
  * you are doing, do not modify this file except to change the parameter class to the startRobot
@@ -21,5 +22,26 @@ public final class Main {
    */
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
+    
+  /*System.out.println("abc");
+  int abc=1;
+  int cba=abc*3;
+  System.out.println(abc+abc*cba);
+  Square square1=new Square(cba);
+  Square square2=new Square(4);
+  System.out.println(square1.getArea());
+  System.out.println(square2.getArea());
+  int[] nums=new int[5];
+  for(int j=0;j<=4;j+=1){
+    nums[j]=j;
+    System.out.println(nums[j]);
   }
-}
+  Square[] squares=new Square[5];
+  for(int j=1;j<=5;j+=1){
+    squares[j-1]=new Square(j);
+  }
+  for(int j=0;j<=4;j+=1){
+    System.out.println(squares[j].getArea());  
+  }
+  }*/
+}}
